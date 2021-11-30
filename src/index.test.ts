1e71f96eeb530f51d50877ce83544ced4ef9c3b3
Hugo Masclet <hugo@lukso.io>
2021-11-30 16:40:17 +0100

    Support all keyType except Bytes20Mapping

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,105 @@
 import { lsp2Parse } from '.';
 
 describe('lsp2parse', () => {
-  it('finds keys of type Singleton correctly', () => {
-    const schema = lsp2Parse(
-      '0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5',
-    );
+  describe('Singleton', () => {
+    it('finds keys of type Singleton correctly', () => {
+      const schema = lsp2Parse(
+        '0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5',
+      );
 
-    expect(schema).toEqual({
-      name: 'LSP3Profile',
-      key: '0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5',
-      keyType: 'Singleton',
-      valueContent: 'JSONURL',
-      valueType: 'bytes',
+      expect(schema).toEqual({
+        name: 'LSP3Profile',
+        key: '0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5',
+        keyType: 'Singleton',
+        valueContent: 'JSONURL',
+        valueType: 'bytes',
+      });
+    });
+  });
+
+  describe('Array', () => {
+    it('finds initial key of type Array correctly', () => {
+      const schema = lsp2Parse(
+        '0x3a47ab5bd3a594c3a8995f8fa58d0876c96819ca4516bd76100c92462f2f9dc0',
+      );
+
+      expect(schema).toEqual({
+        name: 'LSP3IssuedAssets[]',
+        key: '0x3a47ab5bd3a594c3a8995f8fa58d0876c96819ca4516bd76100c92462f2f9dc0',
+        keyType: 'Array',
+        valueContent: 'Address',
+        valueType: 'address',
+      });
+    });
+    it('finds subsequent key of type Array correctly', () => {
+      const schema = lsp2Parse(
+        '0x3a47ab5bd3a594c3a8995f8fa58d087600000000000000000000000000000001',
+      );
+
+      expect(schema).toEqual({
+        name: 'LSP3IssuedAssets[1]',
+        key: '0x3a47ab5bd3a594c3a8995f8fa58d0876c96819ca4516bd76100c92462f2f9dc0',
+        keyType: 'Singleton',
+        valueContent: 'Address',
+        valueType: 'address',
+      });
+    });
+    it('finds subsequent key of type Array correctly', () => {
+      const schema = lsp2Parse(
+        '0x3a47ab5bd3a594c3a8995f8fa58d087600000000000fab000000000000000001',
+      );
+
+      expect(schema).toEqual(null);
+    });
+  });
+
+  describe('Mapping', () => {
+    it('finds known mappings', () => {
+      const schema = lsp2Parse(
+        '0xeafec4d89fa9619884b6b89135626455000000000000000000000000abe425d6',
+      );
+
+      expect(schema).toEqual({
+        name: 'SupportedStandards:LSP3UniversalProfile',
+        key: '0xeafec4d89fa9619884b6b89135626455000000000000000000000000abe425d6',
+        keyType: 'Mapping',
+        valueContent: '0xabe425d6',
+        valueType: 'bytes',
+      });
+    });
+
+    it('finds unknown mappings', () => {
+      // Key name: SupportedStandards:UnknownKey
+      const schema = lsp2Parse(
+        '0xeafec4d89fa9619884b6b89135626455000000000000000000000000f4d7faed',
+      );
+
+      expect(schema).toEqual({
+        name: 'SupportedStandards:??????',
+        key: '0xeafec4d89fa9619884b6b89135626455000000000000000000000000f4d7faed',
+        keyType: 'Mapping',
+        valueContent: '0xabe425d6',
+        valueType: 'bytes',
+      });
+    });
+  });
+  describe('Bytes20Mapping', () => {
+    it.todo('finds Bytes20Mapping');
+  });
+  describe('Bytes20MappingWithGrouping', () => {
+    it('finds Bytes20MappingWithGrouping', () => {
+      const address = 'af3bf2ffb025098b79caddfbdd113b3681817744';
+      const name = `AddressPermissions:Permissions:${address}`;
+      const key = `0x4b80742d0000000082ac0000${address}`;
+      const schema = lsp2Parse(key);
+
+      expect(schema).toEqual({
+        name,
+        key,
+        keyType: 'Bytes20MappingWithGrouping',
+        valueContent: 'BitArray',
+        valueType: 'byte32',
+      });
     });
   });
 });
--- src/parser.ts
+++ src/parser.ts
@@ -1,31 +1,130 @@
+/**
+ * @file parser.ts
+ * @author Hugo Masclet <@Hugoo>
+ * @date 2021
+ */
+
 import { ERC725JSONSchema, ERC725JSONSchemaKeyType } from '@erc725/erc725.js';
-import schemas from './schemas';
+import allSchemas from './schemas';
 
 const getSchemasByKeyType = (): Record<
   ERC725JSONSchemaKeyType,
   ERC725JSONSchema[]
 > => {
   return {
-    Singleton: schemas.filter((schema) => schema.keyType === 'Singleton'),
-    Array: schemas.filter((schema) => schema.keyType === 'Array'),
-    Mapping: schemas.filter((schema) => schema.keyType === 'Mapping'),
-    Bytes20Mapping: schemas.filter(
+    Singleton: allSchemas.filter((schema) => schema.keyType === 'Singleton'),
+    Array: allSchemas.filter((schema) => schema.keyType === 'Array'),
+    Mapping: allSchemas.filter((schema) => schema.keyType === 'Mapping'),
+    Bytes20Mapping: allSchemas.filter(
       (schema) => schema.keyType === 'Bytes20Mapping',
     ),
-    Bytes20MappingWithGrouping: schemas.filter(
+    Bytes20MappingWithGrouping: allSchemas.filter(
       (schema) => schema.keyType === 'Bytes20MappingWithGrouping',
     ),
   };
 };
 
-const findSingletonKey = (
+const findSingletonSchemaForKey = (
   key: string,
-  singletonSchemas: ERC725JSONSchema[],
+  schemas: ERC725JSONSchema[],
 ): ERC725JSONSchema | null => {
-  return (
-    singletonSchemas.find((singletonSchema) => singletonSchema.key === key) ||
-    null
-  );
+  return schemas.find((schema) => schema.key === key) || null;
+};
+
+const findArraySchemaForKey = (
+  key: string,
+  schemas: ERC725JSONSchema[],
+): ERC725JSONSchema | null => {
+  // Should detect:
+
+  // 1. Initial key
+  const initialKeySchema = schemas.find((schema) => schema.key === key) || null;
+
+  if (initialKeySchema) {
+    return initialKeySchema;
+  }
+
+  // 2. Subsequent keys
+  const bytes16Key = key.substr(0, 34);
+  const arraySchema =
+    schemas.find((schema) => schema.key.substr(0, 34) === bytes16Key) || null;
+
+  if (!arraySchema) {
+    return null;
+  }
+
+  const elementIndex = parseInt(key.substr(34), 10);
+
+  if (!elementIndex) {
+    return null;
+  }
+
+  return {
+    ...arraySchema,
+    name: arraySchema.name.replace('[]', `[${elementIndex}]`),
+    keyType: 'Singleton',
+  };
+};
+
+const findMappingSchemaForKey = (
+  key: string,
+  schemas: ERC725JSONSchema[],
+): ERC725JSONSchema | null => {
+  // Should detect:
+
+  // 1. Known/defined mapping
+  let keySchema = schemas.find((schema) => schema.key === key) || null;
+
+  if (keySchema) {
+    return keySchema;
+  }
+
+  // 2. "Semi defined mappings" i.e. "SupportedStandards:??????"
+  keySchema =
+    schemas.find((schema) => schema.key.substr(0, 58) === key.substr(0, 58)) ||
+    null;
+
+  if (!keySchema) {
+    return null;
+  }
+
+  return {
+    ...keySchema,
+    name: `${keySchema.name.split(':')[0]}:??????`,
+    key,
+  };
+};
+
+// TODO:
+const findBytes20MappingSchemaForKey = (
+  key: string,
+  schemas: ERC725JSONSchema[],
+): ERC725JSONSchema | null => {
+  return null;
+};
+
+const findBytes20MappingWithGroupingSchemaForKey = (
+  key: string,
+  schemas: ERC725JSONSchema[],
+): ERC725JSONSchema | null => {
+  const keySchema =
+    schemas.find((schema) => schema.key.substr(0, 26) === key.substr(0, 26)) ||
+    null;
+
+  const address = key.substr(26);
+
+  if (keySchema) {
+    return {
+      ...keySchema,
+      key,
+      name: `${keySchema.name.substr(
+        0,
+        keySchema.name.lastIndexOf(':'),
+      )}:${address}`,
+    };
+  }
+
+  return null;
 };
 
 export function parser(key: string): ERC725JSONSchema | null {
@@ -33,19 +132,37 @@ export function parser(key: string): ERC725JSONSchema | null {
 
   let foundSchema: ERC725JSONSchema | null = null;
 
-  foundSchema = findSingletonKey(key, schemasByKeyType.Singleton);
+  foundSchema = findSingletonSchemaForKey(key, schemasByKeyType.Singleton);
 
   if (foundSchema) {
     return foundSchema;
   }
 
-  // TODO: Array
+  foundSchema = findArraySchemaForKey(key, schemasByKeyType.Array);
 
-  // TODO: Mapping
+  if (foundSchema) {
+    return foundSchema;
+  }
 
-  // TODO: Bytes20Mapping
+  foundSchema = findMappingSchemaForKey(key, schemasByKeyType.Mapping);
 
-  // TODO: Bytes20MappingWithGrouping
+  if (foundSchema) {
+    return foundSchema;
+  }
+
+  foundSchema = findBytes20MappingSchemaForKey(
+    key,
+    schemasByKeyType.Bytes20Mapping,
+  );
+
+  if (foundSchema) {
+    return foundSchema;
+  }
+
+  foundSchema = findBytes20MappingWithGroupingSchemaForKey(
+    key,
+    schemasByKeyType.Bytes20MappingWithGrouping,
+  );
 
   return foundSchema;
 }
